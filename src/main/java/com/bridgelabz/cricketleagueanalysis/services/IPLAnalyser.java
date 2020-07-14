319c6bed42b3caae842477b6ca960fc3d026c834
nikhildhruwe <nick.dhruwe@gmail.com>
2020-07-15 00:53:18 +0530

    Added: Test case to check sorted by highest average batting

--- src/main/java/com/bridgelabz/cricketleagueanalysis/services/IPLAnalyser.java
+++ src/main/java/com/bridgelabz/cricketleagueanalysis/services/IPLAnalyser.java
@@ -3,8 +3,13 @@ package com.bridgelabz.cricketleagueanalysis.services;
 import com.bridgelabz.cricketleagueanalysis.adapter.IPLAdapterFactory;
 import com.bridgelabz.cricketleagueanalysis.dao.IPLAnalyserDAO;
 import com.bridgelabz.cricketleagueanalysis.exception.IPLAnalyserException;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toCollection;
 
 
 public class IPLAnalyser {
@@ -23,7 +28,16 @@ public class IPLAnalyser {
         return iplMap.size();
     }
 
-    public String getMostBattingAverages() {
-        return null;
+    public String getMostBattingAverages() throws IPLAnalyserException {
+        if (iplMap == null || iplMap.size() == 0) {
+            throw new IPLAnalyserException("No iplData"
+                    , IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM);
+        }
+        Comparator<IPLAnalyserDAO> iplComparator = Comparator.comparing(iplData -> iplData.averages);
+        ArrayList iplDTO = iplMap.values().stream().sorted(iplComparator.reversed())
+                .map(iplAnalyserDAO -> iplAnalyserDAO.getIPLDTO(player))
+                .collect(toCollection(ArrayList::new));
+        String sortedIPLJson = new Gson().toJson(iplDTO);
+        return sortedIPLJson;
     }
 }
--- src/test/java/IPLAnalysisTest.java
+++ src/test/java/IPLAnalysisTest.java
@@ -1,5 +1,7 @@
 import com.bridgelabz.cricketleagueanalysis.exception.IPLAnalyserException;
+import com.bridgelabz.cricketleagueanalysis.model.IPLMostRunsData;
 import com.bridgelabz.cricketleagueanalysis.services.IPLAnalyser;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -17,6 +19,20 @@ public class IPLAnalysisTest {
             Assert.assertEquals(100, iplRecordCount);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIPLMostRunsCSVData_WhenSortedWithBattingAverages_ShouldReturnInDecreasingOrder() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser(IPLAnalyser.Player.BATSMAN);
+            iplAnalyser.loadIPLData(IPLAnalyser.Player.BATSMAN, IPL_MOST_RUNS_CSV_FILE_PATH);
+            String mostBattingAverages = iplAnalyser.getMostBattingAverages();
+             IPLMostRunsData[] iplMostRunsData= new Gson().fromJson(mostBattingAverages, IPLMostRunsData[].class);
+            Assert.assertEquals("MS Dhoni", iplMostRunsData[0].player);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM, e.type);
+            System.out.println(e.getMessage());
         }
     }
 }
